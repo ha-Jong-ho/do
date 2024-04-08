eb5c862d7239d2648d927cc3f6daf0237dcd29e1
Ha-Jong-Ho <sifh0044@naver.com>
2024-04-08 18:37:49 +0900

    휴대폰

---
+++ javascript/휴대폰번호.js
@@ -0,0 +1,29 @@
+class UserData{
+  constructor(phoneNum) {
+    this.replaceNumber = (num) => {
+        return num.replaceAll(/[\w]/gi, '*');
+    };
+  	this.setPhoneNum = (phoneNum) => {
+  		this.phoneNum = phoneNum;
+      if (this.phoneNum && this.phoneNum.length > 3) {
+        let div = this.phoneNum.length - 4;
+        let marking_num = this.phoneNum.substring(0, div);
+        let leaving_num = this.phoneNum.substring(div);
+      	this.phoneNum = this.replaceNumber(marking_num) + leaving_num;
+      } else {
+        this.phoneNum = this.replaceNumber(this.phoneNum);
+      }
+    };
+    this.getPhoneNum = () => {
+    	return this.phoneNum;
+    }
+  
+  	this.setPhoneNum(phoneNum);
+  }
+}
+
+
+function solution(phone_number) {
+    let d = new UserData(phone_number);
+    return d.getPhoneNum();
+}
